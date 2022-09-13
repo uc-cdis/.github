d53a6fa82f0982edfe0a9f259c7e85ca1971b12a
Aidan Hilt <11202897+AidanHilt@users.noreply.github.com>
2022-09-13 15:30:30 -0400

    Update fence.sh

--- unit_test_scripts/fence.sh
+++ unit_test_scripts/fence.sh
@@ -1,4 +1,11 @@
 sudo rm -f /etc/boto.cfg
-mkdir -p tests/resources/keys; cd tests/resources/keys; openssl genrsa -out test_private_key.pem 2048; openssl rsa -in test_private_key.pem -pubout -out test_public_key.pem
-openssl genrsa -out test_private_key_2.pem 2048; openssl rsa -in test_private_key_2.pem -pubout -out test_public_key_2.pem
-cd -
+mkdir -p tests/resources/keys 
+
+cd tests/resources/keys 
+openssl genrsa -out test_private_key.pem 2048
+openssl rsa -in test_private_key.pem -pubout -out test_public_key.pem
+
+openssl genrsa -out test_private_key_2.pem 2048
+openssl rsa -in test_private_key_2.pem -pubout -out test_public_key_2.pem
+
+cd
